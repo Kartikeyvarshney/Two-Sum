06758a388b63a61f806684c94d6ede629e96fd8d
Kartikey Varshney <105634736+Kartikeyvarshney@users.noreply.github.com>
2022-10-12 01:38:02 +0530

    Create Two_Sum.java

---
+++ Two_Sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target)
+    {
+        int  output[] = new int[2];
+        for(int i =0 ; i < nums.length ; i++)
+        {   
+            for ( int j = i+1 ; j < nums.length ; j++ )
+            {
+                if (i+1 < nums.length)
+                {
+                    if ( nums[i] + nums[j] == target )
+                    {
+                        output[0] = i;
+                        output[1] = j;
+                        break;
+                    }
+                }
+            }                    
+        }
+        return output;
+        
+    }
+}
